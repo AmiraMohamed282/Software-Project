24b8a8750a249fb985dcf409feae01b87390452e
AmiraMohamed282 <amiramahran321@gmail.com>
2025-05-08 23:20:38 +0300

    Authintication

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-const index = () => {
+export default function index() {
   return (
-    <View>
-      <Text>Hello..</Text>
+    <View style={styles.container}>
+      <ActivityIndicator size={"large"} color={"gray"} />
     </View>
-  )
+  );
 }
 
-export default index
+index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ActivityIndicator: {
+    fontSize: "large",
+  },
+});
